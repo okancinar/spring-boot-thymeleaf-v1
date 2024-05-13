65f1531fb53866e17f9717ced26be2d73200d8af
okancinar <okancinar10189@gmail.com>
2024-05-13 20:44:12 +0300

    v1

---
+++ src/main/resources/data.sql
@@ -0,0 +1,2 @@
+INSERT INTO employees (first_name, last_name, email) VALUES ('Okan', 'Çınar', 'okan.cinar@example.com');
+INSERT INTO employees (first_name, last_name, email) VALUES ('Murat Ege', 'Birtane', 'murat.ege@example.com');
